6cf1fc290390efc9de06ec5bbcb16eef6dec1bd0
Eriko <eriko.918k@gmail.com>
2022-02-20 17:33:40 +0900

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 import './App.css';
 
-function App() {
-  return ( <div className="App"> Weather  App</div> );
+export default function App() {
+  return ( 
+  <div className="App"> 
+  <h1>Weather  App</h1>
+     <footer>
+       This project was coded by me and is {" "}
+        <a 
+         href="https://github.com/eriko1819/react-weather-app-soph"
+         target="_balnk">
+        open-sourced on GitHub</a>
+     </footer> 
+  </div> 
+  );
 }
-
-export default App;
