cb14a6b26686f772271856ec17850609743c5472
Eriko <eriko.918k@gmail.com>
2022-02-28 23:09:32 +0900

    Added useEffect - refresh

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,13 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecsat] = useState(null);
+  
+  useEffect(() => {
+    setLoaded(false);
+    // set loaded false
+  }, [props.coordinates]);  
+    // if the coordinates change - whenever
+   
   
   function handleResponse(response) {
     setForecsat(response.data.daily)
